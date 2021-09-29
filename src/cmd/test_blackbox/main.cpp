f91e117d69436bd697cd9e449bea727b8b9e6abf
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-09-29 13:03:24 -0500

    expand tests

--- src/cmd/test_blackbox/main.cpp
+++ src/cmd/test_blackbox/main.cpp
@@ -20,7 +20,21 @@ static void test_algorithm(
         if (a(n) != std::nullopt) { panic(label, "expected base case nullopt"); }
     }
 
-    if (a(4UL) != std::make_tuple(2UL, 2UL)) { panic(label, "expected 4 -> (2, 2)"); }
+    for (auto n = 4UL; n < 100; n += 2) {
+        if (a(n) != std::make_tuple(2, n / 2)) { panic(label, "expected 2q -> (2, q)"); }
+    }
+
+    blackbox::sieve s{};
+
+    for (auto i = 0; i < 100; i++) {
+        s.grow();
+    }
+
+    for (const auto p : s.primes) {
+        const auto n = p * p;
+        if (a(n) != std::make_tuple(p, p)) { panic(label, "expected p^2 -> (p, p)"); }
+    }
+
     if (a(5UL) != std::nullopt) { panic(label, "expected 5 -> nullopt"); }
     if (a(6UL) != std::make_tuple(2UL, 3UL)) { panic(label, "expected 6 -> (2, 3)"); }
     if (a(7UL) != std::nullopt) { panic(label, "expected 7 -> nullopt"); }
